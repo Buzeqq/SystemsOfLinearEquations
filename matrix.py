d0f6427648ae2146817346158a4371bfe3f2f571
buzeqq <miloszchoj@gmail.com>
2022-05-01 01:36:40 +0200

    Added lu solver with lu decomposition and ultimate comparison

--- matrix.py
+++ matrix.py
@@ -1,4 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor
 from copy import deepcopy
+import time
 
 
 class Matrix:
@@ -10,8 +12,8 @@ class Matrix:
         if n is None:
             n = 0
 
-        self.matrix = [[0 if value is None else value for i in range(n if size is None else size[1])]
-                       for j in range(n if size is None else size[0])]
+        self.matrix = [[0 if value is None else value for _ in range(n if size is None else size[1])]
+                       for _ in range(n if size is None else size[0])]
         self.size = (n, n) if size is None else size
         self.rows = self.size[0]
         self.cols = self.size[1]
@@ -22,30 +24,13 @@ class Matrix:
         self.rows = self.size[0]
         self.cols = self.size[1]
 
-    def set_at(self, i: int, j: int = 0, value=0.0):
-        if not self._check_index_boundaries(i, j):
-            print("Invalid i or j, when setting value in matrix.", i, j)
-            return
-
-        self.matrix[i][j] = value
-
-    def get_at(self, i: int, j: int = 0):
-        if not self._check_index_boundaries(i, j):
-            print("Invalid i or j, when setting value in matrix.", i, j)
-            return
-
-        return self.matrix[i][j]
-
-    def _check_index_boundaries(self, row: int, col: int):
-        return (0, 0) <= (row, col) < self.size
-
     def get_row(self, row):
         return self.matrix[row]
 
     def get_col(self, col):
         res = []
         for i in range(self.size[0]):
-            res.append(self.matrix[i][col])
+            res.append(self[i, col])
         return res
 
     def get_row_sum(self, row):
@@ -54,7 +39,7 @@ class Matrix:
     def get_col_sum(self, col):
         sum_of_col = 0
         for i in range(self.rows):
-            sum_of_col += self.matrix[i][col]
+            sum_of_col += self[i, col]
         return sum_of_col
 
     def head(self, limit):
@@ -67,7 +52,7 @@ class Matrix:
         col_max = float("-inf")
         row_index = 0
         for i in range(start_row, self.rows):
-            value = abs(self.get_at(i, col))
+            value = abs(self[i, col])
             if value > col_max:
                 col_max = value
                 row_index = i
@@ -89,7 +74,7 @@ class Matrix:
             for j in range(self.size[1]):
                 dst = abs(i - j)
                 if i >= j and dst >= distance:
-                    res.set_at(i, j, self.matrix[i][j])
+                    res[i, j] = self[i, j]
         return res
 
     def tri_u(self, distance=0):
@@ -99,11 +84,11 @@ class Matrix:
             for j in range(self.cols):
                 dst = abs(i - j)
                 if i <= j and dst >= distance:
-                    res.set_at(i, j, self.matrix[i][j])
+                    res[i, j] = self[i, j]
         return res
 
     def diag(self):
-        return Matrix(from_list=[[0 if i != j else self.get_at(i, j) for i in range(self.cols)]
+        return Matrix(from_list=[[0 if i != j else self[i, j] for i in range(self.cols)]
                                  for j in range(self.rows)])
 
     def set_col(self, col: int, list_of_new_elements: list):
@@ -111,18 +96,21 @@ class Matrix:
             return
 
         for i in range(self.rows):
-            self.set_at(i, col, list_of_new_elements[i])
+            self[i, col] = list_of_new_elements[i]
 
     def __str__(self):
-        return str(self.head(3)) + " ..." + str(self.tail(3))
+        string = ""
+        for row in self.matrix:
+            string += str(row) + "\n"
+        return string
 
     def __mul__(self, other):
         if isinstance(other, (int, float)):
             res = deepcopy(self)
             for i in range(res.rows):
                 for j in range(res.cols):
-                    val = self.get_at(i, j) * other
-                    res.set_at(i, j, val)
+                    val = self[i, j] * other
+                    res[i, j] = val
             return res
 
         if not isinstance(other, Matrix):
@@ -137,9 +125,33 @@ class Matrix:
             for j in range(res.cols):
                 value = 0
                 for k in range(self.cols):
-                    value += self.matrix[i][k] * other.matrix[k][j]
+                    value += self[i, k] * other[k, j]
 
-                res.set_at(i, j, value)
+                res[i, j] = value
+        return res
+
+    def threaded_mul(self, other):
+        if not isinstance(other, Matrix):
+            return None
+
+        if self.cols != other.rows:
+            print("Cannot mul matrices with given sizes", self.size, other.size)
+            return None
+
+        res = Matrix(size=(self.cols, other.cols))
+
+        def _mul_col(n: int):
+            for j in range(res.cols):
+                value = 0
+                for k in range(self.cols):
+                    value += self[n, k] * other[k, j]
+
+                # print(value)
+                res[n, j] = value
+
+        with ThreadPoolExecutor(max_workers=7) as exe:
+            exe.map(_mul_col, [i for i in range(res.rows)])
+
         return res
 
     def __sub__(self, other):
@@ -147,8 +159,8 @@ class Matrix:
             res = deepcopy(self)
             for i in range(res.rows):
                 for j in range(res.cols):
-                    val = self.get_at(i, j) - other
-                    res.set_at(i, j, val)
+                    val = self[i, j] - other
+                    res[i, j] = val
             return res
 
         if not isinstance(other, Matrix):
@@ -161,16 +173,16 @@ class Matrix:
         res = deepcopy(self)
         for i in range(res.rows):
             for j in range(res.cols):
-                value = self.get_at(i, j) - other.get_at(i, j)
-                res.set_at(i, j, value)
+                value = self[i, j] - other[i, j]
+                res[i, j] = value
         return res
 
     def __neg__(self):
         res = deepcopy(self)
         for i in range(res.rows):
             for j in range(res.cols):
-                val = -self.get_at(i, j)
-                res.set_at(i, j, val)
+                val = -self[i, j]
+                res[i, j] = val
         return res
 
     def __add__(self, other):
@@ -178,8 +190,8 @@ class Matrix:
             res = deepcopy(self)
             for i in range(res.rows):
                 for j in range(res.cols):
-                    val = self.get_at(i, j) + other
-                    res.set_at(i, j, val)
+                    val = self[i, j] + other
+                    res[i, j] = val
             return res
 
         if not isinstance(other, Matrix):
@@ -192,8 +204,7 @@ class Matrix:
         res = deepcopy(self)
         for i in range(res.rows):
             for j in range(res.cols):
-                value = self.get_at(i, j) + other.get_at(i, j)
-                res.set_at(i, j, value)
+                res[i, j] = self[i, j] + other[i, j]
         return res
 
     def __eq__(self, other):
@@ -202,22 +213,28 @@ class Matrix:
 
         for i in range(self.rows):
             for j in range(self.cols):
-                if self.get_at(i, j) != other.get_at(i, j):
+                if self[i, j] != other[i, j]:
                     return False
         return True
+
+    def __setitem__(self, key: (int, int), value):
+        self.matrix[key[0]][key[1]] = value
+
+    def __getitem__(self, key: (int, int)):
+        return self.matrix[key[0]][key[1]]
 
 
 def ones(n):
     ret = Matrix(n)
     for i in range(ret.rows):
-        ret.set_at(i, i, 1.)
+        ret[i, i] = 1.
     return ret
 
 
 def is_diagonal(a: Matrix):
     for i in range(a.rows):
         for j in range(a.cols):
-            if i != j and a.get_at(i, j) != 0:
+            if i != j and a[i, j] != 0:
                 return False
     return True
 
@@ -237,7 +254,7 @@ def diagonal_solver(a: Matrix, b: Matrix):
 
         res = []
         for i in range(a.rows):
-            res.append(col[i] / a.get_at(i, i))
+            res.append(col[i] / a[i, i])
         results.set_col(c, res)
 
     return results
@@ -246,8 +263,8 @@ def diagonal_solver(a: Matrix, b: Matrix):
 def forward_substitution_solver(a: Matrix, b: Matrix):
     for i in range(a.rows):
         for j in range(i + 1, a.cols):
-            if a.get_at(i, j) != 0:
-                print("Matrix A is not triangular lower!")
+            if a[i, j] != 0:
+                print("Matrix A is not triangular lower!", i, j, a[i, j])
                 return None
 
     if a.cols != b.rows:
@@ -261,9 +278,73 @@ def forward_substitution_solver(a: Matrix, b: Matrix):
         res = []
         for i in range(a.rows):
             numerator = col[i]
-            for j in range(0, i):
-                numerator -= a.get_at(i, j) * res[j]
-            res.append(numerator/a.get_at(i, i))
+            for j in range(i):
+                numerator -= a[i, j] * res[j]
+            res.append(numerator / a[i, i])
         results.set_col(c, res)
 
     return results
+
+
+def backward_substitution_solver(a: Matrix, b: Matrix):
+    for i in range(a.rows):
+        for j in range(i):
+            if a[i, j] != 0:
+                print("Matrix A is not triangular upper!")
+                return None
+
+    if a.cols != b.rows:
+        print("Sizes of matrices does not match!")
+        return None
+
+    results = Matrix(size=(b.rows, b.cols))
+    for c in range(b.cols):
+        col = b.get_col(c)
+
+        res = []
+        for i in range(a.rows):
+            numerator = col[a.rows - 1 - i]
+            for j in range(i):
+                numerator -= a[a.rows - 1 - i, a.cols - 1 - j] * res[len(res) - 1 - j]
+            value = numerator / a[a.rows - 1 - i, a.rows - 1 - i]
+            res.insert(0, value)
+        results.set_col(c, res)
+
+    return results
+
+
+def lu_decomposition(a: Matrix):
+    upper = deepcopy(a)
+    lower = ones(a.rows)
+    perm = ones(a.rows)
+
+    for k in range(a.rows - 1):
+        max_row_index = upper.get_max_abs_in_col_below_row(k, k)
+        if max_row_index != k:
+            upper.swap_rows(k, max_row_index, perm)
+            lower.swap_rows(k, max_row_index, n=k)
+
+        for j in range(k + 1, a.rows):
+            value = upper[j, k] / upper[k, k]
+            lower[j, k] = value
+
+            for i in range(k, a.rows):
+                value = upper[j, i] - lower[j, k] * upper[k, i]
+                upper[j, i] = value
+
+    return lower, upper, perm
+
+
+def lu_solver(a: Matrix, b: Matrix):
+    lower, upper, perm = lu_decomposition(a)
+    b = perm * b
+
+    result = Matrix(size=b.size)
+    for c in range(b.cols):
+        col = b.get_col(c)
+
+        lower_res = forward_substitution_solver(lower, Matrix(from_list=[[i] for i in col]))
+        upper_res = backward_substitution_solver(upper, lower_res)
+        result.set_col(c, upper_res.get_col(0))
+
+    return result
